9e9aed24497e5502598d9106feda16f21e77de19
neusoft <neusoft@neusoftdeMini.padavan>
2018-09-12 11:57:59 +0800

    move hellocontroller

--- hello/src/main/java/com/didispace/hello/HelloController.java
+++ hello/src/main/java/com/didispace/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.didispace.web;
+package com.didispace.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
